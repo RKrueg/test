5441ecaabbf79631d27b0a423452cc47affd0541
RKrueg <thegckid@gmail.com>
2023-01-22 02:02:44 -0800

    async, try and catch, and fetch a response from api

---
+++ src/plant_service.js
@@ -0,0 +1,23 @@
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '44cd67c618mshb7b1598b50085f9p1eede7jsn1362f2657e81',
+    'X-RapidAPI-Host': 'house-plants2.p.rapidapi.com'
+  }
+};
+
+export default class PlantService {
+  static async getPlantInfo(plant) {
+    try {
+      const response = await fetch(`https://house-plants2.p.rapidapi.com/?q=${plant}`, options);
+      const jResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jResponse;
+    } catch (error) {
+      return error;
+    }
+  }
+}
