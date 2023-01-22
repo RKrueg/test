ca3dad13aa33ddd2ae1c9f590488d9aaf9d0b5cd
RKrueg <thegckid@gmail.com>
2023-01-22 02:03:30 -0800

    update webpack to allow images

--- webpack.config.js
+++ webpack.config.js
@@ -11,28 +11,43 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'eval-source-map',
-  devServer: {               
-    contentBase: './dist'    
+  devServer: {
+    contentBase: './dist'
   },
   plugins: [
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'test',
       template: './src/index.html',
       inject: 'body'
     }),
     new Dotenv()
   ],
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.css$/,
         use: [
           'style-loader',
           'css-loader'
         ]
-      }
+      },
+      {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/images/'
+          }
+        }]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
     ]
   }
 };
